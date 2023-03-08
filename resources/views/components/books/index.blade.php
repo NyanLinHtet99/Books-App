1a26719fff7cbd2225a9a61cf81a53b792393d1d
xylo <xylonic3@gmail.com>
2023-03-08 17:37:24 +0630

    scroll bug

--- resources/views/components/books/index.blade.php
+++ resources/views/components/books/index.blade.php
@@ -2,121 +2,25 @@
     <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
     <div class="row gx-3">
         @foreach ($books as $book)
-            <div class="col-lg-3 col-md-4 col-sm-6 book" data-index="{{ $loop->index }}">
-                <a href="#">
-                    <div class="content" style="height:200px">
-                        <div class="content-overlay"></div>
-                        <img src="{{ Storage::url('books/' . $book->cover) }}" alt="cover"
-                            class="img-fluid content-image" id="cover" />
-                        <div class="content-details fadeIn-bottom">
-                            <h3 class="fs-6">{{ $book->title }}</h3>
+            <div class="col-lg-3 col-md-4 col-sm-6">
+
+                    <a href="#" class="bookWindowLink" data-index="{{ $loop->index }}">
+                        <div class="content" style="height:200px">
+                            <div class="content-overlay"></div>
+                            <img src="{{ Storage::url('books/' . $book->cover) }}" alt="cover"
+                                class="img-fluid content-image" id="cover" />
+                            <div class="content-details fadeIn-bottom">
+                                <h3 class="fs-6">{{ $book->title }}</h3>
+                            </div>
                         </div>
-                    </div>
-                </a>
+                    </a>
+
+
             </div>
         @endforeach
 
     </div>
     {{ $books->links() }}
-    {{-- <script type="text/javascript">
-        let data;
-        const params = new Proxy(new URLSearchParams(window.location.search), {
-            get: (searchParams, prop) => searchParams.get(prop),
-        });
-        (async function() {
-            $.ajax({
-                url: "/books?page=" + params.page,
-                type: "get",
-                dataType: "json",
-                success: function(response) {
-                    data = response.data;
-                },
-            });
-        })();
-        let booksWindow = (function() {
-            function _createElements(book) {
-                let card = $('<div>').addClass('card', 'w-100');
-                card.css('height', '100%');
-                let image = $('<img>').attr({
-                    'src': "/storage/books/" + book.cover,
-                    'id': 'cover',
-                    'alt': 'cover',
-                    'class': 'mx-auto mt-2'
-                });
-                image.css('width', '200px');
-                let body = $('<div>').addClass('card-body');
-                let title = $('<h5>').addClass('card-title').text(book.title);
-                let description = $('<p>').addClass('card-text').text(book.description);
-                let button = $('<a>').attr({
-                    'href': '#',
-                    'class': 'btn btn-primary'
-                }).text('Read More');
-                body.append(title, description, button);
-                card.append(image, body);
-                $('#booksWindowContent').html(card);
-            };
-
-
-            function _createWindow(offset) {
-                $('#booksWindow').jqxWindow({
-                    position: {
-                        x: offset.left + 170,
-                        y: offset.top + 30
-                    },
-                    showCloseButton: true,
-                    maxHeight: 600,
-                    maxWidth: 600,
-                    minHeight: 300,
-                    minWidth: 300,
-                    height: 500,
-                    width: 400,
-                    autoOpen: true,
-                    animationType: 'combined',
-                    theme: 'light',
-                    initContent: function() {
-                        // $('#tab').jqxTabs({
-                        //     height: '100%',
-                        //     width: '100%'
-                        // });
-                        $('#booksWindow').jqxWindow('focus');
-                    }
-                });
-            };
-            return {
-                config: {
-                    dragArea: null
-                },
-                init: function(book, parent) {
-                    _createElements(book);
-                    _createWindow(parent);
-                }
-            };
-        }());
-        $(".book").each(function() {
-            let book = $(this);
-            book.on('mouseenter', function() {
-                let bool = $('#booksWindow').jqxWindow('isOpen');
-                if (bool) {
-                    $('#booksWindow').jqxWindow('close');
-                }
-            });
-            book.on('click', function() {
-                let index = $(this).attr('data-index');
-                booksWindow.init(data[index], $(this).offset());
-                $('#booksWindow').jqxWindow('open');
-            });
-        });
-        $('#booksWindow').on('mouseleave', function() {
-            $('#booksWindow').jqxWindow('close');
-        });
-
-        // let books = document.querySelectorAll('.book');
-        // books.forEach(book => {
-        //     book.addEventListener("mouseenter", (event) => {
-        //         console.log(e.target);
-        //     });
-        // });
-    </script> --}}
     <div id="books">
         <div>
             <div id="booksWindow">
--- storage/app/public/js/booksWindow.js
+++ storage/app/public/js/booksWindow.js
@@ -2,7 +2,9 @@ let data;
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
-let booksWindow;
+
+
+
 (async function () {
     $.ajax({
         url: "/books?page=" + params.page,
@@ -12,94 +14,97 @@ let booksWindow;
             data = response.data;
         },
     });
-
-    booksWindow = (function () {
-        function _createElements(book) {
-            let card = $("<div>").addClass("card", "w-100");
-            card.css("height", "100%");
-            let image = $("<img>").attr({
-                src: "/storage/books/" + book.cover,
-                id: "cover",
-                alt: "cover",
-                class: "mx-auto mt-2",
-            });
-            image.css("width", "200px");
-            let body = $("<div>").addClass("card-body");
-            let title = $("<h5>").addClass("card-title").text(book.title);
-            let description = $("<p>")
-                .addClass("card-text")
-                .text(book.description);
-            let button = $("<a>")
-                .attr({
-                    href: "#",
-                    class: "btn btn-primary",
-                })
-                .text("Read More");
-            body.append(title, description, button);
-            card.append(image, body);
-            $("#booksWindowContent").html(card);
-        }
-        function _createWindow(book) {
-            let offset = book.offset();
-            let width = $("#booksContainer").width() * 0.6;
-            let height = 500;
-            let minWidth = 300;
-            width = width < minWidth ? 300 : width;
-            let winHeight = offset.top + height;
-            let winWidth = offset.left + width + book.width();
-            let x = offset.left + book.width();
-            let y = offset.top;
-            if (winHeight > $("body").height()) {
-                y = offset.top + book.height() - height;
-            }
-            if (winWidth > $("body").width()) {
-                x = offset.left - width;
-            }
-            $("#booksWindow").jqxWindow({
-                position: {
-                    x: x,
-                    y: y,
-                },
-                showCloseButton: true,
-                maxHeight: 500,
-                maxWidth: 500,
-                minHeight: 300,
-                minWidth: 300,
-                height: height,
-                width: width,
-                animationType: "combined",
-                theme: "light",
-                initContent: function () {
-                    $("#booksWindow").jqxWindow("focus");
-                },
-            });
-        }
-        return {
-            config: {
-                dragArea: null,
-            },
-            init: function (book, parent) {
-                _createElements(book);
-                _createWindow(parent);
-            },
-        };
-    })();
+        $("#booksWindow").jqxWindow({
+            position: 'top, left',
+            autoOpen: 'false',
+            showCloseButton: true,
+            maxHeight: 500,
+            maxWidth: 500,
+            minHeight: 0,
+            minWidth: 0,
+            height: 0,
+            width: 0,
+            animationType: "combined",
+            theme: "light",
+        });
+        $("#booksWindow").jqxWindow("close");
 })();
-$(".book").each(function () {
+function _createElements(book) {
+    let card = $("<div>").addClass("card", "w-100");
+    card.css("height", "100%");
+    let image = $("<img>").attr({
+        src: "/storage/books/" + book.cover,
+        id: "cover",
+        alt: "cover",
+        class: "mx-auto mt-2",
+    });
+    image.css("width", "200px");
+    let body = $("<div>").addClass("card-body");
+    let title = $("<h5>").addClass("card-title").text(book.title);
+    let description = $("<p>")
+        .addClass("card-text")
+        .text(book.description);
+    let button = $("<a>")
+        .attr({
+            href: "",
+            class: "btn btn-primary",
+        })
+        .text("Read More");
+    body.append(title, description, button);
+    card.append(image, body);
+    $("#booksWindow").jqxWindow('setContent', card);
+}
+$(".bookWindowLink").each(function () {
     let book = $(this);
-    book.on("mouseenter", function () {
+    book.on("mousedown", function () {
         let bool = $("#booksWindow").jqxWindow("isOpen");
         if (bool) {
             $("#booksWindow").jqxWindow("close");
         }
     });
-    book.on("click", function () {
+    book.on("mouseup", function () {
         let index = $(this).attr("data-index");
-        booksWindow.init(data[index], $(this));
+        _createElements(data[index]);
+        $('#booksWindow').jqxWindow(changeOffset($(this)));
         $("#booksWindow").jqxWindow("open");
+        $('html, body').animate({
+            scrollTop: $(this).offset().top - 200
+        });
+        // $('#booksWindow').get(0).scrollIntoView({behavior: 'smooth'});
+
         return false;
     });
+});
+$('#booksWindow').on('focus',function(e){
+    e.preventDefault();
 });
 $("#booksWindow").on("mouseleave", function () {
     $("#booksWindow").jqxWindow("close");
 });
+function changeOffset(book) {
+    let offset = book.offset();
+    let width = $("#booksContainer").width() * 0.6;
+    let height = 460;
+    let minWidth = 300;
+    width = width < minWidth ? 300 : width;
+    let winHeight = offset.top + height;
+    let winWidth = offset.left + width + book.width();
+    let x = offset.left + book.width();
+    let y = offset.top;
+    if (winHeight > $("body").height()) {
+        y = offset.top + book.height() - height;
+    }
+    if (winWidth > $("body").width()) {
+        x = offset.left - width;
+    }
+    return {
+        position: {
+            x: x,
+            y: y,
+        },
+        height: height,
+        width: width,
+        minWidth: 350,
+        minHeight: 350,
+    }
+}
